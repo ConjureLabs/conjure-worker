64dbc8677b7f511f53c40ff2b52ca833cfe20363
tmarshall <timothyjmarshall@gmail.com>
2017-09-04 18:28:03 -0700

    +bash dir

---
+++ bash/npm/start.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# Called on `npm start`
+
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+. $BASE/../functions.cfg;
+
+set -e; # die on any error
+
+export NODE_PATH=$(cd $APP_DIR; cd server; pwd);
+source $APP_DIR/.profile;
+
+set +e; # no longer die on any error
+
+( cd $APP_DIR && nodemon --legacy-watch ./server/ ) &
+PIDS[1]=$!;
+announce "App running";
+PIDS[2]=$!;
+# by tracking pids, and using this trap, all tracked processes will be killed after a ^C
+# see http://stackoverflow.com/questions/9023164/in-bash-how-can-i-run-multiple-infinitely-running-commands-and-cancel-them-all
+trap "kill ${PIDS[*]} && wait ${PIDS[*]} 2>/dev/null" SIGINT;
+wait;
