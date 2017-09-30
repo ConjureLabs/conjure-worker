c350e5a150b5ea59a2a300da7af5ef0e42d144f3
tmarshall <timothyjmarshall@gmail.com>
2017-09-29 19:21:17 -0700

    updated template setup

--- lib/git-container/build.sh
+++ lib/git-container/build.sh
@@ -4,14 +4,16 @@ BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 . $BASE/vars.cfg;
 
 # see http://stackoverflow.com/questions/407523/escape-a-string-for-a-sed-replace-pattern
-TARGET_REPO=$(sed 's/[\/&]/\\&/g' <<< $1); # -e?
-TARGET_BRANCH=$(sed 's/[\/&]/\\&/g' <<< $2);
-CONTAINER_NAME=$(sed 's/[\/&]/\\&/g' <<< $3);
-TARGET_PRE_SETUP=$(sed 's/[\/&]/\\&/g' <<< $4);
-TARGET_SETUP=$(sed 's/[\/&]/\\&/g' <<< $5);
+TARGET_TEMPLATE=$(sed 's/[\/&]/\\&/g' <<< $1);
+TARGET_REPO=$(sed 's/[\/&]/\\&/g' <<< $2); # -e?
+TARGET_BRANCH=$(sed 's/[\/&]/\\&/g' <<< $3);
+CONTAINER_NAME=$(sed 's/[\/&]/\\&/g' <<< $4);
+TARGET_PRE_SETUP=$(sed 's/[\/&]/\\&/g' <<< $5);
+TARGET_SETUP=$(sed 's/[\/&]/\\&/g' <<< $6);
 CACHEBUST=$(date +%s);
 
 DOCKERFILE_CONTENT=$(cat "$PROJECT_DIR/template.Dockerfile");
+DOCKERFILE_CONTENT=$(sed "s/<TEMPLATE>/$TARGET_TEMPLATE/g" <<< "$DOCKERFILE_CONTENT");
 DOCKERFILE_CONTENT=$(sed "s/<REPO>/$TARGET_REPO/g" <<< "$DOCKERFILE_CONTENT");
 DOCKERFILE_CONTENT=$(sed "s/<BRANCH>/$TARGET_BRANCH/g" <<< "$DOCKERFILE_CONTENT");
 DOCKERFILE_CONTENT=$(sed "s/<CACHEBUST>/$CACHEBUST/g" <<< "$DOCKERFILE_CONTENT");
@@ -24,6 +26,6 @@ if [ "$TARGET_PRE_SETUP" != "" ]; then
 fi
 DOCKERFILE_CONTENT+=$(echo -e "\nRUN $TARGET_SETUP");
 
-echo "$DOCKERFILE_CONTENT" > "$PROJECT_DIR/current.Dockerfile";
+echo "$DOCKERFILE_CONTENT" > "$TEMP_DOCKERFILE_DIR/$CONTAINER_NAME.Dockerfile";
 
-docker build -t "$CONTAINER_NAME:latest" -f "$PROJECT_DIR/current.Dockerfile" .;
+docker build -t "$CONTAINER_NAME:latest" -f "$TEMP_DOCKERFILE_DIR/$CONTAINER_NAME.Dockerfile" .;
--- lib/git-container/template.Dockerfile
+++ lib/git-container/template.Dockerfile
@@ -1,22 +1,4 @@
-# OS & initial setup
-FROM debian:stable-20170907
-
-# initial
-RUN apt-get update
-RUN apt-get install -y curl apt-transport-https ca-certificates
-RUN apt-get install -y gnupg gnupg2
-
-# install git
-RUN apt-get install -y git
-
-# assuming env is node
-RUN curl -sL https://deb.nodesource.com/setup_6.x | bash -
-RUN apt-get install -y nodejs
-
-# user & env setup
-USER root
-ENV HOME /root
-WORKDIR /var/conjure/code
+FROM <TEMPLATE>
 
 # pull codebase & branch
 # using CACHEBUST to prevent caching of git clone - see https://github.com/moby/moby/issues/1996#issuecomment-185872769
