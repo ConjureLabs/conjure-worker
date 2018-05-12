f27b2a59b83b22cd9e95ffd515c637d0bdbd649e
tmarshall <timothyjmarshall@gmail.com>
2018-05-12 14:40:42 -0700

    use labda function handler setup

--- lambda/heartbeat-monitor/index.js
+++ lambda/heartbeat-monitor/index.js
@@ -1,22 +1,27 @@
 /*
   local run:
-    source ./.hob/.env && node ./lambda/heartbeat-monitor
+    source ./.hob/.env && node
+
+    then
+
+    const l = require('./lambda/heartbeat-monitor')
+    l.handler().then(process.exit())
  */
 
-require('@conjurelabs/utils/process/handle-exceptions')
+module.exports.handler = async function heartbeatMonitor(/* event, context */) {
+  require('@conjurelabs/utils/process/handle-exceptions')
 
-const { init, query, DatabaseRow } = require('@conjurelabs/db')
-const config = require('conjure-core/modules/config')
-const log = require('conjure-core/modules/log')('lambda.heartbeat-monitor')
+  const { init, query, DatabaseRow } = require('@conjurelabs/db')
+  const config = require('conjure-core/modules/config')
+  const log = require('conjure-core/modules/log')('lambda.heartbeat-monitor')
 
-// configure db connection
-init(config.database.pg, {
-  transformCamelCase: true
-}, (sql, args) => {
-  log.dev.info(sql, process.env.NODE_ENV === 'production' && args ? '---REDACTED---' : args)
-})
+  // configure db connection
+  init(config.database.pg, {
+    transformCamelCase: true
+  }, (sql, args) => {
+    log.dev.info(sql, process.env.NODE_ENV === 'production' && args ? '---REDACTED---' : args)
+  })
 
-async function heartbeatMonitor() {
   // heartbeat should only be happening when the container is being _created_
   // not while running or going down
   const flatlineResult = await query(`
@@ -50,5 +55,3 @@ async function heartbeatMonitor() {
 
   log.info(`update${rowCount === 1 ? '' : 's'} done`)
 }
-
-heartbeatMonitor().then(process.exit)
