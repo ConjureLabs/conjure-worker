cc67c321803ee6f7580aaaa644dd714732be5d0a
tmarshall <timothyjmarshall@gmail.com>
2018-05-12 14:31:41 -0700

    fixed up lambda heartbeat check

--- lambda/heartbeat-monitor/index.js
+++ lambda/heartbeat-monitor/index.js
@@ -1,22 +1,36 @@
-const { query } = require('@conjurelabs/db')
+/*
+  local run:
+    source ./.hob/.env && node ./lambda/heartbeat-monitor
+ */
 
+require('@conjurelabs/utils/process/handle-exceptions')
+
+const { init, query, DatabaseRow } = require('@conjurelabs/db')
+const config = require('conjure-core/modules/config')
 const log = require('conjure-core/modules/log')('lambda.heartbeat-monitor')
 
-(async function() {
+// configure db connection
+init(config.database.pg, {
+  transformCamelCase: true
+}, (sql, args) => {
+  log.dev.info(sql, process.env.NODE_ENV === 'production' && args ? '---REDACTED---' : args)
+})
+
+async function heartbeatMonitor() {
   // heartbeat should only be happening when the container is being _created_
   // not while running or going down
-  const now = new Date()
   const flatlineResult = await query(`
     SELECT id
     FROM container
     WHERE ecs_state = 'spinning up'
     AND creation_failed IS FALSE
-    AND creation_heartbeat < $1 - INTERVAL '2 minutes'
-  `, [now])
+    AND creation_heartbeat < NOW() - INTERVAL '2 minutes'
+  `)
 
   const rowCount = flatlineResult.rows.length
 
   if (!rowCount) {
+    log.info('no rows are stale')
     return
   }
 
@@ -24,7 +38,7 @@ const log = require('conjure-core/modules/log')('lambda.heartbeat-monitor')
 
   const batchAll = require('@conjurelabs/utils/Promise/batch-all')
   await batchAll(3, flatlineResult.rows, row => {
-    return row
+    return new DatabaseRow('container', row)
       .set({
         ecsState: 'failed',
         isActive: false,
@@ -35,4 +49,6 @@ const log = require('conjure-core/modules/log')('lambda.heartbeat-monitor')
   })
 
   log.info(`update${rowCount === 1 ? '' : 's'} done`)
-})()
+}
+
+heartbeatMonitor().then(process.exit)
