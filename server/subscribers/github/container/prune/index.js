9601c5d0674b6dc08aa0105cc400cdc6263c7266
tmarshall <timothyjmarshall@gmail.com>
2018-05-20 11:57:51 -0700

    prune containers

---
+++ server/subscribers/github/container/prune/index.js
@@ -0,0 +1,32 @@
+const Queue = require('conjure-core/classes/Queue')
+const log = require('conjure-core/modules/log')('github.container.prune')
+
+const queue = new Queue('container.prune', true)
+
+queue.subscribe(async (jobData, done) => {
+  if (!jobData.content) {
+    log.error(`Job ${jobData.id}, in container.prune, had no body.content`)
+    return done(new Error('Expected jobData.content'))
+  }
+
+  const GitHubWebhookPayload = require('conjure-core/classes/Repo/GitHub/Webhook/Payload')
+  const payload = new GitHubWebhookPayload(jobData.content)
+
+  const Container = require('conjure-core/classes/Container/GitHub')
+  const container = new Container(payload)
+
+  const { orgName, repoName, branch } = payload
+
+  log.info(`pruning github container (${orgName}/${repoName} --> ${branch})`)
+
+  try {
+    await container.prune()
+  } catch(err) {
+    log.error(err)
+    done(err)
+    return
+  }
+
+  log.info(`pruned github container (${orgName}/${repoName} --> ${branch})`)
+  done()
+})
